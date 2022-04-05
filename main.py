b76bd5ccd0028286dbcc61afeb415858d65d566e
Nicholas Bernard <nbernard@albany.edu>
2022-04-04 22:18:38 -0400

    linear support vector classification example basic scatter plot of two features x and y

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ style.use("ggplot")  # graph styling
 
 # Take any entity with features:
 # Two features, the x feature and the y feature:
-x = [3.0, 3.9, 2.1, 9.3, 4.0, 5.9]
-y = [1.6, 6.8, 1.1, 6.6, 5.6, 4.8]
+x = [3.0, 3.9, 2.1, 5.3, 4.0, 3.9]
+y = [1.6, 6.8, 1.1, 6.6, 5.6, 5.8]
 
 plt.scatter(x, y)
 plt.show()
@@ -18,6 +18,18 @@ plt.show()
 X = np.array([[3.0, 1.6],
               [3.9, 6.8],
               [2.1, 1.1],
-              [9.3, 6.6],
+              [5.3, 6.6],
               [4.0, 5.6],
-              [5.9, 4.8]])
+              [3.9, 5.8]])
+
+# Binary classification scheme (classified based on "low" or "high")
+y = [0, 1, 0, 1, 1, 1]
+
+# Create our classifier
+# C defined explicitly here, although default parameter is also 1.0, so this could have been left implicit
+cls = svm.SVC(kernel='linear', C=1.0)
+
+# fit features to their labels
+cls.fit(X, y)
+
+p = cls.predict([4, 6])
