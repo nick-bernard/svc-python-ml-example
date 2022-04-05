f77b0d20dc86836bff93e3307e99140e2edf44e8
Nicholas Bernard <nbernard@albany.edu>
2022-04-04 22:03:38 -0400

    linear support vector classification example basic scatter plot of two features x and y

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Support vector machine (SVM) used as a classifier (SVC)
+# Support vector machine (SVM) used as a classifier (Linear SVC)
 
 import numpy as np  # numpy arrays
 import matplotlib.pyplot as plt
@@ -9,8 +9,15 @@ style.use("ggplot")  # graph styling
 
 # Take any entity with features:
 # Two features, the x feature and the y feature:
-x = [1.0, 5.0, 1.5, 8.0, 1.0, 9.0]
-y = [2.0, 8.0, 1.8, 8.0, 0.6, 11.0]
+x = [3.0, 3.9, 2.1, 9.3, 4.0, 5.9]
+y = [1.6, 6.8, 1.1, 6.6, 5.6, 4.8]
 
 plt.scatter(x, y)
 plt.show()
+
+X = np.array([[3.0, 1.6],
+              [3.9, 6.8],
+              [2.1, 1.1],
+              [9.3, 6.6],
+              [4.0, 5.6],
+              [5.9, 4.8]])
